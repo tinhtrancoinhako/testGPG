f49181a569d67108c8a2807978e5a2dc46640b0a
tinhtrancoinhako <tinh.tran@coinhako.com>
2023-10-05 16:07:11 +0700

    heloo1

--- hello.py
+++ hello.py
@@ -1,2 +1,2 @@
 def my_function():
-    print("Hello, world!  haha olala  11 2 4")  # This line has trailing whitespace.
+    print("Hello, world!  haha olala  11 2 4 5")  # This line has trailing whitespace.
